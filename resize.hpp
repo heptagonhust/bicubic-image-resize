13b397825f0d38d393cf8e570bf0961c02f4c14c
liuxinyi <577086410@qq.com>
2022-11-24 15:50:51 +0800

    resize image by 5x: 117ms save image ../images/CS_5x.jpg

--- resize.hpp
+++ resize.hpp
@@ -8,12 +8,13 @@
 #include <chrono>
 #include "utils.hpp"
 #include <cmath>
-#define N 4
+#define N 2
 float WeightCoeff(float x, float a) {
+  int temp = x*x;
   if (x <= 1) {
-    return 1 - (a + 3) * x * x + (a + 2) * x * x * x;
+    return 1 - (a + 3) * temp + (a + 2) * x * temp;
   } else if (x < 2) {
-    return -4 * a + 8 * a * x - 5 * a * x * x + a * x * x * x;
+    return -4 * a + 8 * a * x - 5 * a * temp + a * x * temp;
   }
   return 0.0;
 }
@@ -21,17 +22,30 @@ float WeightCoeff(float x, float a) {
 void CalcCoeff4x4(float x, float y, float *coeff) {
   const float a = -0.5f;
 
-  float u = x - floor(x);
+  float u = x - floor(x);   
   float v = y - floor(y);
 
   u += 1;
   v += 1;
+  float C;
+  float WeightCoeff_v[4];
+  //for (int k = 0;;k < 4;k++){
+    WeightCoeff_v[0] = WeightCoeff(fabs(v - 0), a);
+    WeightCoeff_v[1] = WeightCoeff(fabs(v - 1), a);
+    WeightCoeff_v[2] = WeightCoeff(fabs(v - 2), a);
+    WeightCoeff_v[3] = WeightCoeff(fabs(v - 3), a);
 
+  //}
   for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      coeff[i * 4 + j] =
-          WeightCoeff(fabs(u - i), a) * WeightCoeff(fabs(v - j), a);
-    }
+    //for (int j = 0; j < 4; j++) {//循环展开
+      C = WeightCoeff(fabs(u - i), a);
+      coeff[i * 4 + 0] = C * WeightCoeff_v[0];
+      coeff[i * 4 + 1] = C * WeightCoeff_v[1];      
+      coeff[i * 4 + 2] = C * WeightCoeff_v[2];
+      coeff[i * 4 + 3] = C * WeightCoeff_v[3];// make use of cache
+
+ 
+    //}
   }
 }
 
@@ -56,7 +70,7 @@ int SubResize(RGBImage src,unsigned char *res,int channels,int resize_rows,int r
   auto check_perimeter = [src](float x, float y) -> bool {
     return x < src.rows - 2 && x > 1 && y < src.cols - 2 && y > 1;
   };
-  int step_x = ratio * src.cols / N + 1;
+  int step_x = ratio * src.cols / N + 1;//步长的计算，+1 消去截断误差的影响（最后一个块会小一点）
   int step_y = ratio * src.rows / N + 1;
   for (register int i = step_x * block_x; i < resize_rows && i < step_x * (block_x + 1); i++) {
     for (register int j = step_y * block_y; j < resize_cols && j < step_y * (block_y + 1); j++) {
@@ -85,7 +99,7 @@ RGBImage ResizeImage(RGBImage src, float ratio){
   for(int block_x=0;block_x < N;block_x++){
     for(int block_y=0;block_y < N;block_y++){
       for(int channel = 0;channel<channels;channel++)
-        MyThread[i++]=std::thread(SubResize,src,res,channels,resize_rows,resize_cols,ratio,block_x,block_y,channel);
+        MyThread[i++]=std::thread(SubResize,src,res,channels,resize_rows,resize_cols,ratio,block_x,block_y,channel);//分配计算任务给多个线程
     }
   }
   for(int i = 0;i < N * N * channels;i++){
